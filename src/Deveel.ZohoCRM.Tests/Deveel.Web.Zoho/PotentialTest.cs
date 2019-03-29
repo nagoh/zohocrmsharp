22cd80cf6989116dbbcf9d2861417a852e5d6a31
Jason Stevens <jason.stevens@partpay.co.nz>
2019-03-29 13:38:35 +1300

    Prevent xml exceptions when transmitting ']]>' in strings.

--- src/Deveel.ZohoCRM.Tests/Deveel.Web.Zoho/PotentialTest.cs
+++ src/Deveel.ZohoCRM.Tests/Deveel.Web.Zoho/PotentialTest.cs
@@ -54,6 +54,95 @@ namespace Deveel.Web.Deveel.Web.Zoho
 
             records = client.BulkUpsertRecords(_potentials);
         }
-    }
 
+        [Test]
+        public void should_send_data_in_cdata_block()
+        {
+            // Arrange
+            var zohoOrder = new ZohoPotential(Guid.NewGuid().ToString());
+            zohoOrder.SetValue("test", "---some-data---");
+
+            // Act
+            var collection = new ZohoEntityCollection<ZohoPotential>{ zohoOrder };
+            
+            // Assert
+            string xml = null;
+            Assert.DoesNotThrow(() => xml = collection.ToXmlString());
+            
+            Assert.That(xml, Is.Not.Null);
+            Assert.That(xml.Contains("<FL val=\"test\"><![CDATA[---some-data---]]></FL>"));
+        }
+
+        [Test]
+        public void should_not_include_null_value()
+        {
+            // Arrange
+            var zohoOrder = new ZohoPotential(Guid.NewGuid().ToString());
+            zohoOrder.SetValue("test", null);
+
+            // Act
+            var collection = new ZohoEntityCollection<ZohoPotential>{ zohoOrder };
+            
+            // Assert
+            string xml = null;
+            Assert.DoesNotThrow(() => xml = collection.ToXmlString());
+            
+            Assert.That(xml, Is.Not.Null);
+            Assert.That(xml.Contains("<FL val=\"test\">"), Is.False);
+        }
+
+        [Test]
+        public void should_create_empty_tag_with_empty_string()
+        {
+            // Arrange
+            var zohoOrder = new ZohoPotential(Guid.NewGuid().ToString());
+            zohoOrder.SetValue("test", "");
+
+            // Act
+            var collection = new ZohoEntityCollection<ZohoPotential>{ zohoOrder };
+            
+            // Assert
+            string xml = null;
+            Assert.DoesNotThrow(() => xml = collection.ToXmlString());
+            
+            Assert.That(xml, Is.Not.Null);
+            Assert.That(xml.Contains("<FL val=\"test\" />"));
+        }
+
+        [Test]
+        public void should_handle_invalid_cdata_string()
+        {
+            // Arrange
+            var zohoOrder = new ZohoPotential(Guid.NewGuid().ToString());
+            zohoOrder.SetValue("test", "---]]>---");  // Cannot have ']]>' inside an XML CDATA block
+
+            // Act
+            var collection = new ZohoEntityCollection<ZohoPotential>{ zohoOrder };
+            
+            // Assert
+            string xml = null;
+            Assert.DoesNotThrow(() => xml = collection.ToXmlString());
+            
+            Assert.That(xml, Is.Not.Null);
+            Assert.That(xml.Contains("<FL val=\"test\"><![CDATA[---]]>]]&gt;<![CDATA[---]]></FL>"), "Should have separate CData blocks with escaped ']]>'");
+        }
+
+        [Test]
+        public void should_handle_multiple_invalid_cdata_string()
+        {
+            // Arrange
+            var zohoOrder = new ZohoPotential(Guid.NewGuid().ToString());
+            zohoOrder.SetValue("test", "]]>---]]>---]]>");  // Cannot have ']]>' inside an XML CDATA block
+
+            // Act
+            var collection = new ZohoEntityCollection<ZohoPotential>{ zohoOrder };
+            
+            // Assert
+            string xml = null;
+            Assert.DoesNotThrow(() => xml = collection.ToXmlString());
+            
+            Assert.That(xml, Is.Not.Null);
+            Assert.That(xml.Contains("<FL val=\"test\">]]&gt;<![CDATA[---]]>]]&gt;<![CDATA[---]]>]]&gt;</FL>"), "Should have separate CData blocks with escaped ']]>'");
+        }
+    }
 }
